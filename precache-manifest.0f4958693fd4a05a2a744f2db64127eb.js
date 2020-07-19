dc70fab98ccb073cb05c3db83b1e17a4fbca4521
l3ftie <nkarodza@gmail.com>
2020-07-19 20:04:31 +0200

    Updates

---
+++ precache-manifest.0f4958693fd4a05a2a744f2db64127eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800a99d92567f7bcd1d54e13c9bcd9ae",
+    "url": "/github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5cbfa5bb2f2f7963d468",
+    "url": "/github.io/monster-rolodex/static/css/main.7c1084b6.chunk.css"
+  },
+  {
+    "revision": "cfceac3283c0ca996338",
+    "url": "/github.io/monster-rolodex/static/js/2.9c424b6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monster-rolodex/static/js/2.9c424b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbfa5bb2f2f7963d468",
+    "url": "/github.io/monster-rolodex/static/js/main.ca67869a.chunk.js"
+  },
+  {
+    "revision": "4f9b849205977bba94e7",
+    "url": "/github.io/monster-rolodex/static/js/runtime-main.3545cb20.js"
+  }
+]);
